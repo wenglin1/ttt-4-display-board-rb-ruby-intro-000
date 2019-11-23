7a65d50aacb3b02552e4b185af9602b9a106fd56
weng lin <wenglin@buffalo.edu>
2019-11-23 07:29:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
-def display_board
+def display_board(board)
   puts "   |   |   "
-       "-----------"
-       "   |   |   "
-       "-----------"
-       "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end # Define display_board that accepts a board and prints # out the current state. display_board display_board(board)
-display_board
+
+board = [" "," "," "," "," "," "," "," "," "]
